6f0c39163002d5e3a0017a9df4a43803bac20b83
zhangtreefish <zhangtreefish@users.noreply.github.com>
2015-10-30 11:35:39 -0500

    chore:add files for the 1st time

---
+++ media.py
@@ -0,0 +1,6 @@
+class Movie():
+     def __init__(self, movie_title, movie_storyline, movie_poster, movie_trailer):
+         self.title = movie_title
+         self.storyline = movie_storyline
+         self.poster_image_url = movie_poster
+         self.trailer_youtube_url = movie_trailer
