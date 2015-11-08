1dc4b11feeb654cab79de3770815aaf97f4161b5
zhangtreefish <zhangtreefish@users.noreply.github.com>
2015-11-08 14:11:11 -0600

    style:edit code to conform to pep8

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # movie-trailer
 
+README
+
 Project 1 of Full Stack Web Developer Nanodegree program at Udacity
 
 Please go to http://zhangtreefish.github.io/movie-trailer/ to see a short list of my favorite films, and click on the poster to view the trailer.
 
-Used objected-oriented Python programing to create the list of films, and use a web server to serve HTML.
+HOW TO
+
+1.Used objected-oriented Python programing to create the list of films (with class defined in media.py, and object instantiated in entertainment.py), used Python's os module to generate html file, and used Python's webbrowser module to serve HTML.
+2.Follow the style guide per https://www.python.org/dev/peps/pep-0008/#indentation; Use the following site to verify the codes for pep8 requirements: http://pep8online.com/
+3.Shorten url using https://goo.gl/
--- entertainment.py
+++ entertainment.py
@@ -1,10 +1,42 @@
 import media
 
-a_river = media.Movie("A River Runs Through It","two brothers growing up in Montana","https://upload.wikimedia.org/wikipedia/en/c/ce/A_river_runs_through_it_cover.jpg","https://www.youtube.com/watch?v=9WHdEZbIRpc")
-up = media.Movie("Up", "A lonely old man and an orphan scout go on a journey in a balllon-hoisted house","https://upload.wikimedia.org/wikipedia/en/0/05/Up_%282009_film%29.jpg","https://www.youtube.com/watch?v=qas5lWp7_R0")
-kung_fu_panda = media.Movie("Kung Fu Panda","Po and the rest of his Furious-Five gang defend ancient China from a villain", "https://upload.wikimedia.org/wikipedia/en/7/76/Kungfupanda.jpg", "https://www.youtube.com/watch?v=PXi3Mv6KMzY")
-chicken_run = media.Movie("Chicken Run","A cocky American rooster saves the coup from turning into chicken pies","https://upload.wikimedia.org/wikipedia/en/0/00/Chicken_run_ver1.jpg","https://www.youtube.com/watch?v=jVdlxwX6A7g")
-dr_zhivago = media.Movie("Dr. Zhivago", "from Russia in early twentieth century","https://upload.wikimedia.org/wikipedia/en/6/64/DrZhivago_Asheet.jpg","https://www.youtube.com/watch?v=wAWrXTn5Www")
-modern_times = media.Movie("Modern Times", "Charlie Chaplin illuminates industrialization", "https://upload.wikimedia.org/wikipedia/en/6/6e/Moderntimes.jpg","https://www.youtube.com/watch?v=GLeDdzGUTq0")
-ratatouille = media.Movie("Rata Touille", "About the beauty of food and life in simplicity", "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg","https://www.youtube.com/watch?v=c3sBBRxDAqk")
-movies = [a_river, up,kung_fu_panda,chicken_run,dr_zhivago, modern_times,ratatouille ]
+a_river = media.Movie(
+    "A River Runs Through It",
+    "two brothers growing up in Montana",
+    "https://upload.wikimedia.org/wikipedia/en/c/ce/A_river_runs_through_it_\
+        cover.jpg",  # noqa
+    "https://www.youtube.com/watch?v=9WHdEZbIRpc")  # noqa
+up = media.Movie(
+    "Up",
+    "A lonely old man and an orphan scout go on a journey in a balllon-hoisted \
+        house",
+    "https://upload.wikimedia.org/wikipedia/en/0/05/Up_%282009_film%29.jpg",
+    "https://www.youtube.com/watch?v=qas5lWp7_R0")  # noqa
+kung_fu_panda = media.Movie(
+    "Kung Fu Panda",
+    "Po and the rest of his Furious-Five gang defend ancient China from a \
+        villain",
+    "https://upload.wikimedia.org/wikipedia/en/7/76/Kungfupanda.jpg",  # noqa
+    "https://www.youtube.com/watch?v=PXi3Mv6KMzY")  # noqa
+chicken_run = media.Movie(
+    "Chicken Run",
+    "A cocky American rooster saves the coup from turning into chicken pies",
+    "https://upload.wikimedia.org/wikipedia/en/0/00/Chicken_run_ver1.jpg",
+    "https://www.youtube.com/watch?v=jVdlxwX6A7g")  # noqa
+dr_zhivago = media.Movie(
+    "Dr. Zhivago",
+    "an epic from Russia in early twentieth century",
+    "https://goo.gl/qlcwEI",  # use goo.gl to shorten url
+    "https://www.youtube.com/watch?v=wAWrXTn5Www")  # noqa
+modern_times = media.Movie(
+    "Modern Times",
+    "Charlie Chaplin illuminates industrialization",
+    "https://upload.wikimedia.org/wikipedia/en/6/6e/Moderntimes.jpg",  # noqa
+    "https://www.youtube.com/watch?v=GLeDdzGUTq0")  # noqa
+ratatouille = media.Movie(
+    "Rata Touille",
+    "About the beauty of food and life in simplicity",
+    "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+    "https://www.youtube.com/watch?v=c3sBBRxDAqk")  # noqa
+movies = [a_river, up, kung_fu_panda, chicken_run, dr_zhivago, modern_times,
+          ratatouille]
